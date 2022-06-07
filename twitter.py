72882384822f42233fb8365a953a8856a20e90e1
Cesar Romero <zaesarpo@gmail.com>
2022-06-07 10:02:46 +0200

    Twitter autmoate like

---
+++ twitter.py
@@ -0,0 +1,21 @@
+import tweepy
+import time
+
+auth = tweepy.OAuthHandler("API_KEY HERE","API_SECRET HERE")
+auth.set_access_token("ACCESS_TOKEN HERE","ACCESS_TOKEN_SECRET HERE")
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+
+search = "#crypto OR #nft"
+nrTweets = 500
+
+for tweet in tweepy.Cursor(api.search_tweets, search).items(nrTweets):
+    try:
+        print("Tweet Liked")
+        tweet.favorite()
+        time.sleep(60)
+    except tweepy.TweepyError as e:
+        print(e.reason)
+    except StopIteration:
+        break
